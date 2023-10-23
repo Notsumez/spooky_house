0da2fdb55bf79593ee42c43fbaac158a5740e511
marcos <mv663140@gmail.com>
2023-10-23 10:16:44 -0300

    Fontes novas e header

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
     <title>Spooky House</title>
 </head>
 <body id="fundo_index">
+    <!-- Header do index -->
     <?php include 'header.php'; ?>
+    <!-- Footer do index -->
+    <?php include 'footer.php'; ?>
 </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
