e403065ba79ec648fc6b1b679b5dd1da43fbef2a
Andrey <adahorapirola@gmail.com>
2023-10-24 12:16:40 -0300

    fonte drip-octuber adicionada

--- conteudo.php
+++ conteudo.php
@@ -2,8 +2,8 @@
 <div class="container d-flex justify-content-center align-items-center inflar" style="flex-direction: column;" >
     <img src="images/abobora_elemento.png" alt="Elemento Abobora Jack">
     <h1 class="text-light" id="txt_doces">!?DOCES OU TRAVESSURAS!?</h1>
-    <a href="explorar.php" class="text-light btn_explore">EXPLORE AGORA!!</a>
     <p class="text-light" id="txt_motivacional">Entre no espírito da festa, vista sua 'fantasia' e mergulhe no mundo mágico que criamos para você!</p>
+    <a href="explorar.php" class="text-light btn_explore">EXPLORE AGORA!!</a>
     <br>
 </div>
 
