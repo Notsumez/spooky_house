847becbd14118f37e57a0030e8e7a176950673a8
Andrey <adahorapirola@gmail.com>
2023-10-23 22:33:03 -0300

    correção de erro na logo

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
     <div class="container">
         <!-- Logo -->
         <a class="navbar-brand" id="logo_header" href="index.php">
-            <img src="images/halloween" width="7%" alt="Logo Spooky House"> 
+            <img src="images/halloween.svg" width="7%" alt="Logo Spooky House"> 
             Spooky House
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
