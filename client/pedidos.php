d159845e2bff9ba2ca84750eea26a95501a1da49
Bya <beaanunciato39@gmail.com>
2023-10-28 19:17:52 -0300

    trabalho

--- client/pedidos.php
+++ client/pedidos.php
@@ -41,28 +41,23 @@
                         <th style="color: white;">Produto</th>
                         <th style="color: white;">Resumo</th>
                         <th style="color: white;">Preço</th>
-                        <th style="color: white;">Adição</th>
+                        <th style="color: white;">Status</th>
                         <th style="color: white;">Quantidade</th>
-                        <th style="color: white;">Imagem</th>
-                        <th class="d-flex">
-                            <a href="adicionar_carrinho.php" target="_self" class="btn btn-block btn-xs" style="background-color: #38B6FF;" role="button">
-                                <ion-icon name="add-circle-outline"></ion-icon>
-                                <span class="hidden-xs">ADICIONAR</span>
-                            </a>
-                        </th>
+                        <th style="color: white;">Data</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
                     <!-- Estrutura de Repetição -->
                     <?php do {?>
                         <tr>
-                            <td hidden><?php echo $row_carrinho['id'];?></td>
-                            <td style="color: white;"><?php echo $row_carrinho['nome_produto'];?></td>
-                            <td style="color: white;"><?php echo $row_carrinho['resumo'];?></td>
-                            <td style="color: white;"><?php echo $row_carrinho['preco'];?></td>
-                            <td style="color: white;"><?php echo $row_carrinho['data_adicao'];?></td>
-                            <td style="color: white;"><?php echo $row_carrinho['quantidade'];?></td>
-                            <td style="color: white;"><img src="../images/Fantasias/<?php echo $row_carrinho['img_produto'];?>" style="max-width: 50px;" alt=""></td>
+                            <td hidden><?php echo $row['id'];?></td>
+                            <td style="color: white;"><?php echo $row[''];?></td>
+                            <td style="color: white;"><?php echo $row['resumo'];?></td>
+                            <td style="color: white;"><?php echo $row['preco'];?></td>
+                            <td style="color: white;"><?php echo $row['data_adicao'];?></td>
+                            <td style="color: white;"><?php echo $row['quantidade'];?></td>
+                            <td style="color: white;"><img src="../images/Fantasias/<?php echo $row['img_produto'];?>" style="max-width: 50px;" alt=""></td>
                             <td>
                                 <a data-toggle="modal" data-target="#modal_remover_item" role="button" class="btn btn-block btn-xs" style="background-color: #c4302b; margin-bottom: 10px;"> 
                                     <ion-icon name="refresh-outline"></ion-icon>
