6f668baf206de1a2a93eaba7f9735e8d98749630
Bya <beaanunciato39@gmail.com>
2023-10-28 16:21:45 -0300

    tela notificacoes

--- client/notificacoes.php
+++ client/notificacoes.php
@@ -6,10 +6,6 @@
                 <img src="images/halloween.svg" width="7%" alt="Logo Spooky House"> 
                 Spooky House
             </a>
-            <a class="navbar-brand" id="logo_header" href="../index.php">
-                <img src="../images/halloween.svg" width="7%" alt="Logo Spooky House"> 
-                Spooky House
-            </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
