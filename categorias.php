5ea666d39e15fabb33ab08d0089c82799240cd12
Andrey <adahorapirola@gmail.com>
2023-10-29 18:18:21 -0300

    ajustes

--- categorias.php
+++ categorias.php
@@ -53,7 +53,7 @@
                         </div>
 
                         <div class="front-content">
-                            <small class="badge">Pasta</small>
+                            <small class="badge">Categoria</small>
                             <div class="description">
                                 <div class="title">
                                     <p class="title">
@@ -64,7 +64,7 @@
                                     <svg fill-rule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style="mix-blend-mode: normal" text-anchor="none" font-size="none" font-weight="none" font-family="none" stroke-dashoffset="0" stroke-dasharray="" stroke-miterlimit="10" stroke-linejoin="miter" stroke-linecap="butt" stroke-width="1" stroke="none" fill-rule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z"></path></g></g></svg>
                                 </div>
                                 <p class="card-footer">
-                                    <?php echo $row_categoria['descricao'];?>
+                                    <?php echo $row_categoria['descri'];?>
                                 </p>
                             </div>
                         </div>
